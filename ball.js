cf11bfe2398b494c6ff22f9ab657600854570d10
Scott <sbferguson17@gmail.com>
2022-07-27 18:15:53 -0600

    create get/set functions in ball.js

--- ball.js
+++ ball.js
@@ -1,8 +1,24 @@
-import { update } from "tar"
+
 
 export default class Ball {
     constructor(ballElem) {
-        this.ballElem = ballElem
+        this.ballElem = ballElem;
+    }
+
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+    }
+
+    set x(value) {
+        this.ballElem.style.setProperty("--x", value);
+    }
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+    }
+
+    set y(value) {
+        this.ballElem.style.setProperty("--y", value);
     }
 
     update(delta) {
